aa5719cb118e848b9ccc4af709d4fa0f33ab288c
haemin4738 <leeheamin12@gmail.com>
2025-09-03 10:38:38 +0900

    Init

---
+++ src/test/java/com/back/Restapi0903ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restapi0903ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
