c5c30c71b3c14e1201deb45ba20b89c96d5d8d9d
haemin4738 <leeheamin12@gmail.com>
2025-09-04 12:24:50 +0900

    22

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -4,12 +4,12 @@ import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,12 +39,33 @@ public class ApiV1PostController {
     }
 
     @Transactional
-    @GetMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     public RsData<PostDto> delete(@PathVariable Long id) {
         Post post = postService.findById(id);
 
         postService.delete(post);
 
         return new RsData<>("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
+    }
+
+    record PostWriteForm(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String title,
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String content
+    ){}
+
+    @PostMapping
+    @Transactional
+    public RsData<PostDto> write(@Valid @RequestBody PostWriteForm form) {
+        Post post = postService.create(form.title, form.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 게시글이 생성되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
     }
 }
