b4701965eb354b773b5f9f1aa79b55ba218949c7
haemin4738 <leeheamin12@gmail.com>
2025-09-09 15:32:12 +0900

    48

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.back.global.globalExceptionHandler;
 
 import com.back.global.rsData.RsData;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -50,6 +51,18 @@ public class GlobalExceptionHandler {
                 new RsData<>(
                         "400-1",
                         message
+                ),
+                BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<RsData<Void>> handle(HttpMessageNotReadableException e) {
+
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "400-1",
+                        "요청 본문 형식이 올바르지 않습니다."
                 ),
                 BAD_REQUEST
         );
