57280daa6a7cf1764ceb5d031ca34a8d279d580d
haemin4738 <leeheamin12@gmail.com>
2025-09-16 10:34:34 +0900

    글 작성 예외 테스트

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -2,111 +2,326 @@ package com.back.domain.post.post.controller;
 
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
-import com.back.domain.post.post.dto.PostDto;
-import com.back.domain.post.post.dto.PostModifyReqBody;
-import com.back.domain.post.post.dto.PostWriteReqBody;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
-import com.back.global.exception.ServiceException;
-import com.back.global.rsData.RsData;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.RequiredArgsConstructor;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController // @Controller + @ResponseBody
-@Validated
-@RequestMapping("/api/v1/posts")
-@RequiredArgsConstructor
-@Tag(name="ApiV1PostController", description = "API 글 컨트롤러")
-public class ApiV1PostController {
-    private final PostService postService;
-    private final MemberService memberService;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-    @Transactional(readOnly = true)
-    @GetMapping
-    @Operation(summary = "다건 조회")
-    public List<PostDto> getItems() {
-        List<Post> items = postService.getList();
+@ActiveProfiles("test") // 테스트 환경에서는 test 프로파일을 활성화합니다.
+@SpringBootTest // 스프링부트 테스트 클래스임을 나타냅니다.
+@AutoConfigureMockMvc // MockMvc를 자동으로 설정합니다.
+@Transactional // 각 테스트 메서드가 종료되면 롤백됩니다.
+public class ApiV1PostControllerTest {
+    @Autowired
+    private MockMvc mvc; // MockMvc를 주입받습니다.
 
-        return items
-                .stream()
-                .map(PostDto::new) // postDto 변환
-                .toList();
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private MemberService memberService;
+
+    //글쓰기 테스트
+    @Test
+    @DisplayName("글 쓰기")
+    void t1() throws Exception {
+        Member member = memberService.findByUsername("user1").get();
+
+        String apiKey = member.getApiKey();
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .header("Authorization", "Bearer " + apiKey)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "제목",
+                                            "content": "내용"
+                                        }
+                                        """)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        Post post = postService.findLatest().get();
+
+        // 201 Created 상태코드 검증
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 게시글이 생성되었습니다.".formatted(post.getId())))
+                .andExpect(jsonPath("$.data.id").value(post.getId()))
+                .andExpect(jsonPath("$.data.createDate").value(Matchers.startsWith(post.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.modifyDate").value(Matchers.startsWith(post.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.authorName").value(post.getAuthor().getNickname()))
+                .andExpect(jsonPath("$.data.title").value("제목"))
+                .andExpect(jsonPath("$.data.content").value("내용"));
+
     }
 
-    @Transactional(readOnly = true)
-    @GetMapping("/{id}")
-    @Operation(summary = "단건 조회")
-    public PostDto getItem(@PathVariable Long id) {
-        Post item = postService.findById(id);
+    //글쓰기 제목 누락 테스트
+    @Test
+    @DisplayName("글 쓰기 400 - 제목 누락")
+    void t7() throws Exception {
+        Member member = memberService.findByUsername("user1").get();
 
-        return new PostDto(item);
+        String apiKey = member.getApiKey();
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + apiKey)
+                                .content("""
+                                        {
+                                            "title": "",
+                                            "content": "내용"
+                                        }
+                                        """)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        // 400 BadRequest 상태코드 검증
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("""
+                        title-NotBlank-must not be blank
+                        title-Size-size must be between 2 and 100
+                        """.stripIndent().trim()));
     }
 
-    @Transactional
-    @DeleteMapping("/{id}")
-    @Operation(summary = "삭제")
-    public RsData<PostDto> delete(@PathVariable Long id) {
+    //글쓰기 내용 누락 테스트
+    @Test
+    @DisplayName("글 쓰기 400 - 내용 누락")
+    void t8() throws Exception {
+        Member member = memberService.findByUsername("user1").get();
+
+        String apiKey = member.getApiKey();
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + apiKey)
+
+                                .content("""
+                                        {
+                                            "title": "제목",
+                                            "content": ""
+                                        }
+                                        """)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        // 400 BadRequest 상태코드 검증
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("""
+                        content-NotBlank-must not be blank
+                        content-Size-size must be between 2 and 2000
+                        """.stripIndent().trim()));
+    }
+
+    //글쓰기 JSON 문법 에러 테스트
+    @Test
+    @DisplayName("글 쓰기 400 - JSON 문법 에러")
+    void t9() throws Exception {
+        Member member = memberService.findByUsername("user1").get();
+
+        String apiKey = member.getApiKey();
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + apiKey)
+                                .content("""
+                                        {
+                                            "title": "제목",
+                                            content": "내용"
+                                        }
+                                        """)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        // 400 BadRequest 상태코드 검증
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("요청 본문 형식이 올바르지 않습니다."));
+    }
+
+    //글 수정 테스트
+    @Test
+    @DisplayName("글 수정")
+    void t2() throws Exception {
+        long id = 1;
+
         Post post = postService.findById(id);
 
-        postService.delete(post);
+        String apiKey = post.getAuthor().getApiKey();
 
-        return new RsData<>("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        put("/api/v1/posts/" + id)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + apiKey)
+                                .content("""
+                                        {
+                                            "title": "제목 update",
+                                            "content": "내용 update"
+                                        }
+                                        """)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+        // 200 Ok 상태코드 검증
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("modify"))
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 게시글이 수정되었습니다.".formatted(id)));
+
+//        Post post = postService.findById(id);
+//
+//        assertThat(post.getTitle().equals("제목 update"));
+//        assertThat(post.getContent().equals("내용 update"));
     }
 
-    @PostMapping
-    @Transactional
-    @Operation(summary = "작성")
-    public RsData<PostDto> write(
-            @Valid @RequestBody PostWriteReqBody reqBody,
-            @NotBlank @Size(min = 2, max = 50) @RequestHeader("Authorization") String authorization
-    ) {
-        String apiKey = authorization.replace("Bearer ", "");
-
-        Member author = memberService.findByApiKey(apiKey)
-                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 회원입니다."));
-
-        Post post = postService.create(author, reqBody.title(), reqBody.content());
-
-        return new RsData<>(
-                "201-1",
-                "%d번 게시글이 생성되었습니다.".formatted(post.getId()),
-                new PostDto(post)
-        );
-    }
-
-    @PutMapping("/{id}")
-    @Transactional
-    @Operation(summary = "수정")
-    public RsData<Void> modify(
-            @PathVariable long id,
-            @Valid @RequestBody PostModifyReqBody reqBody,
-            @NotBlank @Size(min = 2, max = 50) @RequestHeader("Authorization") String authorization
-
-    ) {
-        String apiKey = authorization.replace("Bearer ", "");
-
-        Member author = memberService.findByApiKey(apiKey)
-                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 회원입니다."));
+    @Test
+    @DisplayName("글 삭제")
+    void t3() throws Exception {
+        long id = 1;
 
         Post post = postService.findById(id);
-        postService.update(post, reqBody.title(), reqBody.content());
 
-        if (!author.equals(post.getAuthor())) {
-            throw new ServiceException("403-1", "글 수정 권한이 없습니다.");
+        String apiKey = post.getAuthor().getApiKey();
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        delete("/api/v1/posts/" + id)
+                                .header("Authorization", "Bearer " + apiKey)
+                )
+
+                .andDo(print()); // 응답을 출력합니다.
+
+        // 200 Ok 상태코드 검증
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("delete"))
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 게시글이 삭제되었습니다.".formatted(id)));
+    }
+
+    @Test
+    @DisplayName("글 단건조회")
+    void t4() throws Exception {
+        long id = 1;
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/posts/" + id)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        Post post = postService.findById(id);
+
+        // 200 Ok 상태코드 검증
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(jsonPath("$.id").value(post.getId()))
+                .andExpect(jsonPath("$.createDate").value(Matchers.startsWith(post.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.modifyDate").value(Matchers.startsWith(post.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.authorName").value(post.getAuthor().getNickname()))
+                .andExpect(jsonPath("$.title").value(post.getTitle()))
+                .andExpect(jsonPath("$.content").value(post.getContent()));
+    }
+
+    @Test
+    @DisplayName("글 단건조회, 404")
+    void t6() throws Exception {
+        long id = Integer.MAX_VALUE;
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/posts/" + id)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        // 404 NotFound상태코드 검증
+        resultActions
+                .andExpect(status().isNotFound())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("getItem"));
+    }
+
+
+    @Test
+    @DisplayName("글 다건조회")
+    void t5() throws Exception {
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/posts")
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        List<Post> posts = postService.getList();
+
+
+        // 200 Ok 상태코드 검증
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("getItems"))
+                .andExpect(jsonPath("$.length()").value(posts.size()));
+
+        for (int i =0; i < posts.size(); i++) {
+            Post post = posts.get(i);
+
+            resultActions
+                    .andExpect(jsonPath("$[%d].id".formatted(i)).value(post.getId()))
+                    .andExpect(jsonPath("$[%d].createDate".formatted(i)).value(Matchers.startsWith(post.getCreateDate().toString().substring(0, 20))))
+                    .andExpect(jsonPath("$[%d].modifyDate".formatted(i)).value(Matchers.startsWith(post.getModifyDate().toString().substring(0, 20))))
+                    .andExpect(jsonPath("$[%d].authorName".formatted(i)).value(post.getAuthor().getNickname()))
+                    .andExpect(jsonPath("$[%d].title".formatted(i)).value(post.getTitle()))
+                    .andExpect(jsonPath("$[%d].content".formatted(i)).value(post.getContent()));
         }
-
-        return new RsData<>(
-                "200-1",
-                "%d번 게시글이 수정되었습니다.".formatted(id)
-        );
     }
 }
