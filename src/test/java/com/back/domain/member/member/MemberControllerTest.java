c7bb7841a540faf61c93e03a872562577c3c8d2b
haemin4738 <leeheamin12@gmail.com>
2025-09-17 14:22:20 +0900

    Cookie헤더 인증정보 처리

--- src/test/java/com/back/domain/member/member/MemberControllerTest.java
+++ src/test/java/com/back/domain/member/member/MemberControllerTest.java
@@ -1,69 +1,170 @@
-package com.back.domain.member.member.controller;
+package com.back.domain.member.member;
 
-import com.back.domain.member.member.dto.MemberDto;
-import com.back.domain.member.member.dto.MemberJoinReqBody;
-import com.back.domain.member.member.dto.MemberLoginReqBody;
-import com.back.domain.member.member.dto.MemberLoginResBody;
+
+import com.back.domain.member.member.controller.ApiV1MemberController;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
-import com.back.global.Rq.Rq;
-import com.back.global.exception.ServiceException;
-import com.back.global.rsData.RsData;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.Cookie;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
-@RestController
-@RequestMapping("/api/v1/members")
-@Reqq
-@Tag(name="ApiV1MemberController", description = "API 맴버 컨트롤러")
-public class ApiV1MemberController {
-    private final MemberService memberService;
-    private final Rq rq;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-    @PostMapping
-    public RsData<MemberDto> join(@Valid @RequestBody MemberJoinReqBody reqBody) {
-        Member member = memberService.join(reqBody.username(), reqBody.password(), reqBody.nickname());
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class MemberControllerTest {
 
-        return new RsData<>(
-                "201-1",
-                "%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getNickname()),
-                new MemberDto(member)
+    @Autowired
+    private MockMvc mvc; // MockMvc를 주입받습니다.
+
+    @Autowired
+    private MemberService memberService;
+
+
+    @Test
+    @DisplayName("회원가입")
+    void t1() throws Exception {
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/members")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                                "username": "usernew",
+                                                "password": "1234",
+                                                "nickname": "유저new"
+                                        }
+                                        """)
+                )
+                .andDo(print());
+
+        Member member = memberService.findByUsername("usernew").get();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getNickname())))
+                .andExpect(jsonPath("$.data.id").value(member.getId()))
+                .andExpect(jsonPath("$.data.createDate").value(Matchers.startsWith(member.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.nickname").value(member.getNickname()));
+    }
+
+
+    @Test
+    @DisplayName("로그인")
+    void t2() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/members/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "username": "user1",
+                                            "password": "1234"
+                                        }
+                                        """.stripIndent())
+                )
+                .andDo(print());
+
+        Member member = memberService.findByUsername("user1").get();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("login"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%s님 환영합니다.".formatted(member.getNickname())))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.item").exists())
+                .andExpect(jsonPath("$.data.item.id").value(member.getId()))
+                .andExpect(jsonPath("$.data.item.createDate").value(Matchers.startsWith(member.getCreateDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.data.item.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.data.item.nickname").value(member.getNickname()))
+                .andExpect(jsonPath("$.data.apiKey").value(member.getApiKey()));
+
+        resultActions.andExpect(
+                result -> {
+                    Cookie apiKeyCookie= result.getResponse().getCookie("apiKey");
+                    assertThat(apiKeyCookie.getValue()).isNotBlank();
+                    assertThat(apiKeyCookie.getPath()).isEqualTo("/");
+                    assertThat(apiKeyCookie.getAttribute("HttpOnly")).isEqualTo("true");
+                }
         );
     }
 
-    @PostMapping("/login")
-    public RsData<MemberLoginResBody> login(
-            @Valid @RequestBody MemberLoginReqBody reqBody
-    ) {
-        Member member = memberService.findByUsername(reqBody.username())
-                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 회원입니다."));
+    @Test
+    @DisplayName("내 정보")
+    void t3() throws Exception {
+        Member actor = memberService.findByUsername("user1").get();
+        String apiKey = actor.getApiKey();
 
-        if (!member.getPassword().equals(reqBody.password())) {
-            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
-        }
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/members/me")
+                                .header("Authorization", "Bearer " + apiKey)
+                )
+                .andDo(print());
 
-        rq.setCookie("apiKey", member.getApiKey());
+        Member member = memberService.findByUsername("user1").get();
 
-        return new RsData<>(
-                "200-1",
-                "%s님 환영합니다.".formatted(member.getNickname()),
-                new MemberLoginResBody(
-                        new MemberDto(member),
-                        member.getApiKey())
-        );
-
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("me"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%s님 정보입니다.".formatted(member.getNickname())))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id").value(member.getId()))
+                .andExpect(jsonPath("$.data.createDate").value(Matchers.startsWith(member.getCreateDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.data.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.data.nickname").value(member.getNickname()));
     }
 
-    @GetMapping("/me")
-    public RsData<MemberDto> me() {
-        Member actor = rq.getAuthor();
+    @Test
+    @DisplayName("내 정보, with apiKey Cookie")
+    void t4() throws Exception {
+        Member actor =  memberService.findByUsername("user1").get();
+        String apiKey = actor.getApiKey();
 
-        return new RsData(
-                "200-1",
-                "%s님 정보입니다.".formatted(actor.getNickname()),
-                new MemberDto(actor)
-        );
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/members/me")
+                                .cookie(new Cookie("apiKey", apiKey))
+                )
+                .andDo(print());
+
+        Member member = memberService.findByUsername("user1").get();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("me"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%s님 정보입니다.".formatted(member.getNickname())))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id").value(member.getId()))
+                .andExpect(jsonPath("$.data.createDate").value(Matchers.startsWith(member.getCreateDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.data.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.data.nickname").value(member.getNickname()));
     }
+
 }
