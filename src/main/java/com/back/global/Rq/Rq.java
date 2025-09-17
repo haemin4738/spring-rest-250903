079b8599f8ce029d406180e95265477218f2a890
haemin4738 <leeheamin12@gmail.com>
2025-09-17 12:33:34 +0900

    쿠키관련 코드 Rq로 이동

--- src/main/java/com/back/global/Rq/Rq.java
+++ src/main/java/com/back/global/Rq/Rq.java
@@ -2,19 +2,24 @@ package com.back.global.Rq;
 
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
+import com.back.domain.post.post.service.PostService;
 import com.back.global.exception.ServiceException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class Rq {
+    private final PostService postService;
     private final HttpServletRequest req;
+    private final HttpServletResponse resp;
     private final MemberService memberService;
 
     public Member getAuthor() {
-        String headerAuthorization = req.getHeader("Authorization");
+        String headerAuthorization =  req.getHeader("Authorization");
 
         if (headerAuthorization == null || headerAuthorization.isBlank()) {
             throw new ServiceException("401-1", "로그인 후 사용해주세요.");
@@ -26,8 +31,18 @@ public class Rq {
 
         String apiKey = headerAuthorization.substring("Bearer ".length()).trim();
 
-        return memberService.findByApiKey(apiKey)
+        Member member = memberService
+                .findByApiKey(apiKey)
                 .orElseThrow(() -> new ServiceException("401-3", "회원을 찾을 수 없습니다."));
+
+        return member;
+    }
+
+    public void setCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/"); // 쿠키를 도메인 전체에서 쓰겠다.
+        cookie.setHttpOnly(true); // 쿠키를 스크립트로 접근 못하게(XSS 공격방어)
+
+        resp.addCookie(cookie);
     }
 }
-
